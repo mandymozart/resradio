b0b7316a76da3f42fe3ec35749ee93d5f9e64252
Mandy Mozart <mandymozart@viennastruggle.com>
2022-01-17 20:09:27 +0100

    Change title

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-
-import { PrismicRichText, useFirstPrismicDocument } from '@prismicio/react'
-
 import ShowList from './Components/Shows/ShowList';
 import EventList from './Components/Events/EventList';
 import Announcement from './Components/Announcement copy/Announcement';
 import About from './Components/About/About';
 
 function App() {
-  const [document] = useFirstPrismicDocument()
-
   return (
     <div>
       <Announcement/>
-      <img src={logo} className="App-logo" alt="logo" />
-      {document && (
-        <PrismicRichText field={document.data.example_rich_text} />
-      )}
+      <h1>res.radio</h1>
       <EventList/>
       <ShowList/>
       <About/>
