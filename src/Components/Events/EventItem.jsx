26ad6b30bd5b211fc82287fdfd3f8662f2fcd7e2
Mandy Mozart <mandymozart@viennastruggle.com>
2022-01-21 00:42:41 +0100

    Added more compact desktop variant for event listing

--- src/Components/Events/EventItem.jsx
+++ src/Components/Events/EventItem.jsx
@@ -1,17 +1,34 @@
 import styled from "@emotion/styled";
+import clsx from "clsx";
 import React from "react";
+import { isDesktop } from "react-device-detect";
 import { Link } from "react-router-dom";
 import Divider from "../Divider";
 import KeyFieldParagraph from "../KeyFieldParagraph";
 import TeaserImage from "../TeaserImage/TeaserImage";
 import Timeslots from "../Timeslots/Timeslots";
 
-const Container = styled.div``;
+const Container = styled.div`
+  .isDesktop {
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    & > div {
+        width: 20rem;
+    }
+    & > h3 {
+        flex: 100%;
+    }
+  }
+
+`;
 
 const EventItem = ({ event }) => {
   return (
-    <Container>
-      <Link to={event.url}>
+    <Container >
+      <Link to={event.url} className={clsx({isDesktop: isDesktop})}>
         <TeaserImage image={event.data.image} />
         <h3>{event.data.title}</h3>
       </Link>
