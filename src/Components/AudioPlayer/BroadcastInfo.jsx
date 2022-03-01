23fb35e26db619ee68e8eb95ee748ced7a28760a
Mandy Mozart <mandymozart@viennastruggle.com>
2022-03-01 17:31:01 +0100

    Refetch new broadcasts every minute

--- src/Components/AudioPlayer/BroadcastInfo.jsx
+++ src/Components/AudioPlayer/BroadcastInfo.jsx
@@ -3,27 +3,30 @@ import * as prismic from "@prismicio/client";
 import { PrismicLink } from "@prismicio/react";
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
+import utc from "dayjs/plugin/utc";
 import React, { useEffect, useState } from "react";
-import { BrowserView } from "react-device-detect";
 import Marquee from "react-double-marquee";
 import useBroadcastStore from "../../Stores/BroadcastStore";
 import { client } from "./../../prismic";
 dayjs.extend(isBetween);
+dayjs.extend(utc);
 
 const Container = styled.div`
   margin-left: 1rem;
   line-height: 1rem;
   text-align: center;
-  width: 300px;
-  @media only screen and (max-width: 600px) {
-    width: 90px;
+  .info {
+    width: 300px;
+    @media only screen and (max-width: 600px) {
+      width: 90px;
+    }
+    white-space: nowrap;
+    overflow: hidden;
+    /* > div {
+      display: inline-flex; */
+    text-transform: capitalize;
+    /* } */
   }
-  white-space: nowrap;
-  overflow: hidden;
-  /* > div {
-    display: inline-flex; */
-  text-transform: capitalize;
-  /* } */
 `;
 
 const BroadcastInfo = () => {
@@ -33,8 +36,8 @@ const BroadcastInfo = () => {
   const [notFound, toggleNotFound] = useState(false);
 
   const isLive = (begin, end) => {
-    console.log(begin, dayjs().format(), end);
     return dayjs().isBetween(dayjs(begin), dayjs(end));
+    // return true;
   };
 
   // Fetch the Prismic initial Prismic content on page load
@@ -55,30 +58,22 @@ const BroadcastInfo = () => {
           if (data.results_size > 0) {
             // set all
             all = data.results;
-
-            // find current
-            data.results
-              .slice(
-                0,
-                data.results.findIndex((b) => isLive(b.data.begin, b.data.end))
-              )
-              .forEach((b) => {
-                current = b;
-                console.log(b.data);
-              });
+            current = data.results.find((b) =>
+              isLive(b.data.begin, b.data.end)
+            );
 
             // is Now ?
           } else {
             console.log("no broadcasts fetched");
           }
-          console.log(current);
-          console.log(all);
           setBroadcasts(all);
           setCurrentBroadcast(current);
           if (!current) toggleNotFound(true);
         });
     };
     fetchPrismicContent();
+    const intervalId = setInterval(() => fetchPrismicContent(), 60 * 1000);
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
@@ -100,21 +95,17 @@ const BroadcastInfo = () => {
         </>
       )} */}
       {currentBroadcast ? (
-        <>
-          <PrismicLink field={currentBroadcast}>{currentBroadcast.data.title}</PrismicLink>
-          <BrowserView>
-            &nbsp; &mdash; &nbsp;{" "}
+        <div className="info">
+          <Marquee>
             <PrismicLink field={currentBroadcast.data.hostedby}>
-              <Marquee>{currentBroadcast.data.hostedby.slug}</Marquee>
+              {dayjs(currentBroadcast.data.begin).format("HH:mm")} -{" "}
+              {dayjs(currentBroadcast.data.end).format("HH:mm")}:{" "}
+              {currentBroadcast.data.hostedby.uid}
             </PrismicLink>
-          </BrowserView>
-        </>
+          </Marquee>
+        </div>
       ) : (
-        <>
-          {notFound
-            ? ""
-            : "..."}
-        </>
+        <>{notFound ? "" : "..."}</>
       )}
     </Container>
   );
