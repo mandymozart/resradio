8726b63949bec97ac34b6be5fa98ffc96a4865f3
Mandy Mozart <mandymozart@viennastruggle.com>
2022-09-23 10:34:23 +0200

    change order of days

--- src/Components/Events/EventList.jsx
+++ src/Components/Events/EventList.jsx
@@ -3,13 +3,20 @@ import React from "react";
 import EventItem from "./EventItem";
 
 const EventList = () => {
-  const [documents] = usePrismicDocumentsByType("events");
+  const [documents] = usePrismicDocumentsByType("events", {
+    orderings: {
+      field: 'my.events.begin',
+      direction: 'desc',
+    },
+  });
   if (!documents) return <></>;
   return (
     <div>
-      {documents?.results.map((event) => (
-        <EventItem event={event} key={event.id} />
-      ))}
+      {documents?.results.map((event) => {
+        // if (dayjs(event.data.begin).format("MMM D, h:mm A"))
+        // TODO: sort by date here
+        return <EventItem event={event} key={event.id} />;
+      })}
     </div>
   );
 };
