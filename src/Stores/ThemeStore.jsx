f3bec3e8eec47733a92f3914c8c0cee7d8e0da4d
Mandy Mozart <mandymozart@viennastruggle.com>
2023-04-27 22:42:43 +0200

    remove mouse position provider

--- src/Stores/ThemeStore.jsx
+++ src/Stores/ThemeStore.jsx
@@ -4,22 +4,10 @@ import create from "zustand";
 
 const useThemeStore = create(
   produce((set) => ({
-    keyword: "vienna",
-    setKeyword: (keyword) => set((state) => (state.keyword = keyword)),
-    mousePosition: { x: 0, y: 0 },
     nightMode: false,
     setNightMode: (value) => set((state) => {
       state.nightMode = value;
     }),
-    showGifs: true,
-    setShowGifs: (value) => set((state) => {
-      state.showGifs = value;
-    }),
-    setMousePosition: (pos) => {
-      return set((state) => {
-        state.mousePosition = pos;
-      })
-    },
   }))
 );
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { ApolloProvider } from "@apollo/client";
 import { nanoid } from "nanoid";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import App from "./App";
-import MousePositionProvider from "./Components/MousePositionProvider";
 import NotFound from "./Components/NotFound";
 import ScrollToTop from "./Components/ScrollToTop";
 import AboutPage from "./Pages/About";
@@ -34,114 +33,112 @@ configureAbly({ key: "-UKVOQ.551SOg:ATV3cBDTx4wFI1GZpLbCH0EynLqak8T74g85L2o-JJA"
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <MousePositionProvider>
-        <Router>
-          <ScrollToTop>
-            <Routes>
-              <Route exact path="/"
-                element={<App />}>
-                <Route
-                  exact
-                  path="/"
-                  element={
-                    <LandingPage />
-                  }
-                />
-                <Route
-                  exact
-                  path="/explore"
-                  element={
-                    <Explore />
-                  }
-                />
-                <Route
-                  exact
-                  path="/broadcasts/:uid"
-                  element={
-                    <BroadcastPage />
-                  }
-                />
-                <Route
-                  path="/schedule"
-                  element={
-                    <SchedulePage />
-                  }
-                />
-                <Route
-                  path="/schedule/:from"
-                  element={
-                    <SchedulePage />
-                  }
-                />
-                <Route
-                  exact
-                  path="/event/:uid"
-                  element={
-                    <Event />
-                  }
-                />
-                <Route
-                  exact
-                  path="/search/*"
-                  element={
-                    <SearchResults />
-                  }
-                />
-                <Route
-                  exact
-                  path="/shows"
-                  element={
-                    <Shows />
-                  }
-                />
-                <Route
-                  exact
-                  path="/shows/:uid"
-                  element={
-                    <Show />
-                  }
-                />
-                <Route
-                  exact
-                  path="/page/:uid"
-                  element={
-                    <Page />
-                  }
-                />
-                <Route
-                  exact
-                  path="/about"
-                  element={
-                    <AboutPage />
-                  }
-                />
-                <Route
-                  exact
-                  path="/shop"
-                  element={
-                    <ShopPage />
-                  }
-                />
+      <Router>
+        <ScrollToTop>
+          <Routes>
+            <Route exact path="/"
+              element={<App />}>
+              <Route
+                exact
+                path="/"
+                element={
+                  <LandingPage />
+                }
+              />
+              <Route
+                exact
+                path="/explore"
+                element={
+                  <Explore />
+                }
+              />
+              <Route
+                exact
+                path="/broadcasts/:uid"
+                element={
+                  <BroadcastPage />
+                }
+              />
+              <Route
+                path="/schedule"
+                element={
+                  <SchedulePage />
+                }
+              />
+              <Route
+                path="/schedule/:from"
+                element={
+                  <SchedulePage />
+                }
+              />
+              <Route
+                exact
+                path="/event/:uid"
+                element={
+                  <Event />
+                }
+              />
+              <Route
+                exact
+                path="/search/*"
+                element={
+                  <SearchResults />
+                }
+              />
+              <Route
+                exact
+                path="/shows"
+                element={
+                  <Shows />
+                }
+              />
+              <Route
+                exact
+                path="/shows/:uid"
+                element={
+                  <Show />
+                }
+              />
+              <Route
+                exact
+                path="/page/:uid"
+                element={
+                  <Page />
+                }
+              />
+              <Route
+                exact
+                path="/about"
+                element={
+                  <AboutPage />
+                }
+              />
+              <Route
+                exact
+                path="/shop"
+                element={
+                  <ShopPage />
+                }
+              />
 
-                <Route path="*" element={<NotFound />} />
-              </Route>
-              <Route path="studio" element={<Sandbox />}>
-                <Route
-                  path="playlists"
-                  element={
-                    <Playlists />
-                  } />
-                <Route
-                  path="playlists/:uid"
-                  element={
-                    <Player />
-                  } />
-              </Route>
               <Route path="*" element={<NotFound />} />
-            </Routes>
-          </ScrollToTop>
-        </Router>
-      </MousePositionProvider>
+            </Route>
+            <Route path="studio" element={<Sandbox />}>
+              <Route
+                path="playlists"
+                element={
+                  <Playlists />
+                } />
+              <Route
+                path="playlists/:uid"
+                element={
+                  <Player />
+                } />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </ScrollToTop>
+      </Router>
     </ApolloProvider>
   </React.StrictMode >
 );
