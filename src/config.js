f478608dace4c0ef47570e6f55b0ad2beb6ac74f
Mandy Mozart <mandymozart@viennastruggle.com>
2023-05-05 10:46:16 +0200

    responsive recent

--- src/config.js
+++ src/config.js
@@ -5,7 +5,10 @@ const config = {
     OFFLINE_URL: "https://soundimage.org/wp-content/uploads/2016/08/Street-Ambience.mp3",
 }
 
-export const BREAKPOINT = 800;
+export const BREAKPOINT_L = 1279;
+export const BREAKPOINT_MD = 769;
+export const BREAKPOINT_XS = 481;
+
 export const IDENTITY_URL = "https://res-stage.netlify.com";
 
 export default config;
