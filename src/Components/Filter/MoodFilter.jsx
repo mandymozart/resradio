4120f7cc9b9406db8bbd800b7ba9f25502e0d2ea
Mandy Mozart <mandymozart@viennastruggle.com>
2023-05-05 11:46:31 +0200

    mood responsive

--- src/Components/Filter/MoodFilter.jsx
+++ src/Components/Filter/MoodFilter.jsx
@@ -1,12 +1,22 @@
 import { useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
 import { GetMoodsQuery } from "../../Queries/moods";
+import { BREAKPOINT_L, BREAKPOINT_MD, BREAKPOINT_XS } from "../../config";
 import SectionLoader from "../SectionLoader";
 import Mood from "./Mood";
 
 const Container = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr;
+@media (max-width: ${BREAKPOINT_L}px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+@media (max-width: ${BREAKPOINT_MD}px) {
+      grid-template-columns: 1fr 1fr ;
+    }
+@media (max-width: ${BREAKPOINT_XS}px) {
+      grid-template-columns: 1fr;
+    }
 gap: 2rem;
 padding-bottom: 2rem;
 `;
