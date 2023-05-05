7213990259fbf9c402e752fb5f7167d333f2392d
Mandy Mozart <mandymozart@viennastruggle.com>
2023-05-05 13:04:16 +0200

    format

--- src/Components/AudioPlayer/StreamShortInfo.jsx
+++ src/Components/AudioPlayer/StreamShortInfo.jsx
@@ -16,8 +16,8 @@ dayjs.extend(utc);
 
 const Container = styled.div`
 white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+overflow: hidden;
+text-overflow: ellipsis;
 .now {
   color: var(--second);
 }
