dc6704e49bd69523baa1f9edeab3d1b8c7b67a6f
Mandy Mozart <mandymozart@viennastruggle.com>
2023-07-21 08:52:54 +0200

    player black

--- src/images/ClearSmall.jsx
+++ src/images/ClearSmall.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+const Container = styled.svg``
 
 const ClearSmall = () => {
   return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <Container width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1 1L15 15" stroke="currentColor" strokeLinecap="round" />
       <path d="M1 15L15 1" stroke="currentColor" strokeLinecap="round" />
-    </svg>)
+    </Container>)
 }
 
 export default ClearSmall;
