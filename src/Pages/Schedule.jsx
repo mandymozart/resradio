91f99148aa6848bf76b2a3f6dbeab33b00b8f50f
Mandy Mozart <mandymozart@viennastruggle.com>
2023-10-05 16:14:45 +0200

    fixes #15

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -2,10 +2,11 @@ import styled from "@emotion/styled";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import React from "react";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "../Components/Button";
 import Schedule from "../Components/Schedule/Schedule";
-import { BREAKPOINT_L, BREAKPOINT_MD, BREAKPOINT_XS, DATE_FORMAT } from "../config";
+import { BREAKPOINT_L, BREAKPOINT_MD, BREAKPOINT_XS } from "../config";
 dayjs.extend(utc);
 
 const Container = styled.div`
@@ -31,14 +32,17 @@ const Container = styled.div`
     justify-content: space-between;
     gap: 2rem;
     align-items: center;
-    padding: 2rem;
+    padding: 0;
     button {
         background: transparent;
         line-height: 2rem;
         box-sizing: border-box;
-        color: var(--second);
-        padding: 0 1rem;
-        border-color: var(--second);
+        padding: 2rem;
+        font-size: 2rem;
+    }
+    span {
+        font-size: 1rem;
+        text-transform: uppercase;
     }
     @media (max-width: ${BREAKPOINT_L}px) {
         grid-template-columns: repeat(3, minmax(0,1fr));
@@ -46,15 +50,6 @@ const Container = styled.div`
     @media (max-width: ${BREAKPOINT_MD}px) {
         span {
             display: none;
-        }
-        button {
-            padding: 0;
-            width: calc(3rem + 2px);
-            height: calc(3rem + 2px);
-            text-align: center;
-            align-items: center;
-            display: inline-flex;
-            justify-content: center;
         }
     }
 }
@@ -82,9 +77,9 @@ const SchedulePage = () => {
                 &nbsp;
             </div>
             <div className="controls">
-                <Button large onClick={() => gotoPreviousWeek()}>&laquo;&nbsp;<span>{previous.format(DATE_FORMAT)}</span></Button>
-                <div>{current.format("DD.MM")}&mdash;{current.add(6, "days").format("DD.MM")}.<span>{current.add(6, "days").format("YYYY")}</span></div>
-                <Button large onClick={() => gotoNextWeek()}><span>{next.format(DATE_FORMAT)}{" "}</span>&nbsp;&raquo;</Button>
+                <Button ghost large onClick={() => gotoPreviousWeek()}><GoChevronLeft /><span>&nbsp;back</span></Button>
+                <div></div>
+                <Button ghost large onClick={() => gotoNextWeek()}><span>next&nbsp;</span><GoChevronRight /></Button>
             </div>
         </Container>
     )
